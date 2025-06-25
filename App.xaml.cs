482d408bb5fb3aa82fdbb5c00e5631566d868db6
ilonaBabakina20 <ilichka.b.2020@yandex.ru>
2025-06-25 20:35:14 +0300

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp3
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
+}
